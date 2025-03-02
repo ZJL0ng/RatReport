a05caad5cf0f579ef00d2c9bfbb02d78165428e7
Jennifer Long <zlongjen@gmail.com>
2025-03-02 06:20:27 -0500

    modified:   src/App.css 	modified:   src/ratport.js

--- src/ratport.js
+++ src/ratport.js
@@ -3,29 +3,31 @@ export default function Ratport() {
         <div className="min-h-screen bg-[#fdfbf3]">
             {/* Header */}
             <header className="bg-orange-600 p-4 flex justify-between items-center">
-                <h1 className="text-white text-3xl font-bold">Ratport</h1>
+                <h1 className="text-white text-5xl font-bold">Ratport</h1>
                 <nav className="flex gap-4">
                     <a href="#" className="text-white">About</a>
                     <a href="#" className="text-white">Resources</a>
-                    <button className="bg-white text-black px-4 py-2 rounded border">Report Sighting</button>
+                    {/*<button className="bg-white text-black px-4 py-2 rounded border">Report Sighting</button>*/}
                 </nav>
             </header>
 
             {/* Main Content */}
-            <main className="p-8 text-left">
+            <main className="p-8  text-left">
                 <h2 className="text-lg">Now viewing stats from:</h2>
                 <h1 className="text-5xl font-bold">Cambridge</h1> {/* IF GOOGLE API WORKS: change to reflect detected location */}
 
-                <div className="mt-6 flex justify-center">
+                <div className="mt-6 mb-6 flex justify-right">
+                    <a href="https://311.boston.gov/tickets/new" target="_blank">
                     <button className="bg-blue-600 text-white px-6 py-3 rounded-lg">Report Sighting
                     </button>
+                    </a>
                 </div>
 
                 {/* Statistic Boxes */}
                 {/* TO XINYI: two flex boxes for the charts, 
                     no need to hardcode image dimensions :3 */}
-                <div className="flex justify-center items-center min-h-70">
-                    <div className="flex gap-4 w-4/5">
+                <div className="flex justify-center items-center min-h-40">
+                    <div className="flex gap-4 w-7/8">
                         <img
                             src="https://sigma-docs-screenshots.s3.us-west-2.amazonaws.com/Workbooks/Visualizations/Build+a+Bar+Chart/bar-chart.png"
                             alt="Image 1"
@@ -41,10 +43,10 @@ export default function Ratport() {
 
 
                 {/* Links */}
-                <div className="mt-6 flex justify-center gap-4">
-                    <button className="border border-blue-600 text-black px-6 py-3 rounded">Your local trash pickup route & schedule</button>
-                    <button className="border border-blue-600 text-black px-6 py-3 rounded">Learn about local disposal guidelines</button>
-                    <button className="border border-blue-600 text-black px-6 py-3 rounded">Do more with BOS:311</button>
+                <div className="mt-6 flex justify-center gap-4 w-7/8">
+                    <button className="border bg-blue-600 border-blue-600 text-white px-6 py-3 rounded">Your local trash pickup route & schedule</button>
+                    <button className="border bg-blue-600 border-blue-600 text-white px-6 py-3 rounded">Learn about local disposal guidelines</button>
+                    <button className="border bg-blue-600 border-blue-600 text-white px-6 py-3 rounded">Do more with BOS:311</button>
                 </div>
             </main>
         </div>
